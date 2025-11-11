f5f5105e3cc2bd1548b70b3bfcf18c352605c521
RON-000000 <schwaro4@stundents.zhaw.ch>
2025-11-11 15:51:12 +0100

    Added text on Home page

---
+++ Movie-Watchlist/src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<div class="container">
+    <h1>Movie Watchlist</h1>
+    <p>Füge auf der "Movies" Seite Filme hinzu, die du gerne sehen möchtest.</p>
+</div>
+
+<style>
+    h1, p {
+        color: white;
+    }
+</style>
